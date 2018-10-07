563db4fad338be1c515ecaab7640def2c4139ca9
Buck DeFore <bdefore@gmail.com>
2018-10-07 14:34:17 +0000

    add minimist and chalk for formal args, cleaner logging

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-console.log('args', process.argv)
-
 var express = require('express'),
-    steam   = require('./index');
+    steam   = require('./index'),
+    parseArgs = require('minimist'),
+    chalk = require('chalk')
 
+var args = parseArgs(process.argv.slice(2))
 var app = express();
-var apiKey = process.argv[3];
-var host = process.argv[4];
-var returnUrl = process.argv[5];
-var secret = process.argv[6];
 
-var port = host.substr(host.lastIndexOf(':') + 1);
+var config = {
+  apiKey: args.apiKey,
+  host: args.authHost,
+  returnUrl: args.returnUrl,
+  secret: args.sessionSecret
+}
 
-app.use(require('express-session')({ resave: false, saveUninitialized: false, secret }));
+console.log(chalk.green('Starting Steam Auth Server'));
+console.log(chalk.yellow(`Config: ${JSON.stringify(config, null, 2)}`))
+
+var port = config.host.substr(config.host.lastIndexOf(':') + 1);
+
+app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: config.secret }));
 app.use(steam.middleware({
-  realm: host, 
-  verify: `${host}/verify`,
-  apiKey: apiKey
+  realm: config.host,
+  verify: `${config.host}/verify`,
+  apiKey: config.apiKey
 }));
 
 app.get('/', function(req, res) {
@@ -27,7 +34,7 @@ app.get('/authenticate', steam.authenticate(), function(req, res) {
 });
 
 app.get('/verify', steam.verify(), function(req, res) {
-  res.redirect(`${returnUrl}?user=${JSON.stringify(req.user)}`)
+  res.redirect(`${config.returnUrl}?user=${JSON.stringify(req.user)}`)
 });
 
 app.get('/logout', steam.enforceLogin('/'), function(req, res) {
@@ -36,4 +43,4 @@ app.get('/logout', steam.enforceLogin('/'), function(req, res) {
 });
 
 app.listen(port);
-console.log(`Listening at ${host}`, port);
+console.log(chalk.green(`Listening at ${config.host}`));
